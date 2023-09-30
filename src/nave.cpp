97e36b5c3af75da91f88640956f3f903ee4c9261
João Victor <91631521+JVictor011@users.noreply.github.com>
2023-09-30 13:04:55 -0300

    Disparo

--- src/nave.cpp
+++ src/nave.cpp
@@ -2,26 +2,28 @@
 
 namespace spc
 {
-    nave::nave() : rotationAngle(0.0f), rotationSpeed(0.5f) {
+    nave::nave() : rotationAngle(0.0f), rotationSpeed(0.5f)
+    {
         // Inicializa as coordenadas do triângulo
-        vertices[0] = -0.5f; vertices[1] = -0.5f; vertices[2] = 0.0f;
-        vertices[3] = 0.5f;  vertices[4] = -0.5f; vertices[5] = 0.0f;
-        vertices[6] = 0.0f;  vertices[7] = 0.5f;  vertices[8] = 0.0f;
+        vertices[0] = -0.5f;
+        vertices[1] = -0.5f;
+        vertices[2] = 0.0f;
+        vertices[3] = 0.5f;
+        vertices[4] = -0.5f;
+        vertices[5] = 0.0f;
+        vertices[6] = 0.0f;
+        vertices[7] = 0.5f;
+        vertices[8] = 0.0f;
     }
-    
+
     void nave::draw()
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glLoadIdentity();
-        glScalef(0.1f, 0.1f, 0.1f);
-        glRotatef(getRotationAngle(), 0.0f, 0.0f, 1.0f);
-
-        // Desenha o triângulo
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glVertexPointer(3, GL_FLOAT, 0, vertices);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDisableClientState(GL_VERTEX_ARRAY);
+        glBegin(GL_TRIANGLES);
+        glColor3f(0.0f, 0.0f, 1.0f); // Cor azul
+        glVertex2f(-0.025f, -0.025f);
+        glVertex2f(0.025f, -0.025f);
+        glVertex2f(0.0f, 0.025f);
+        glEnd();
     }
 
     void nave::updatePosition(GLFWwindow *window)
@@ -43,5 +45,17 @@ namespace spc
 
         // Ajusta a velocidade de rotação
         rotationAngle *= rotationSpeed;
+    }
+
+    void nave::disparoPosition(double drawPoint, double mouseX, double mouseY){
+        if (drawPoint)
+        {
+            glPointSize(50);
+            glBegin(GL_POINTS);
+            glColor3f(1.0f, 0.0f, 0.0f);                                    // Cor vermelha
+            glVertex2d((mouseX - 540.0) / 540.0, (540.0 - mouseY) / 540.0); // Mapeia a posição do ponto para as coordenadas do OpenGL
+            glEnd();
+            glPointSize(1.0f);
+        }
     }
 }
